114e062e01833597ebe601acd70a01c6591e2256
Wallace Carr <wallacecarr4@gmail.com>
2019-08-08 10:47:35 -0600

    add dayDecisionManager.go

---
+++ dayDecisionManager.go
@@ -0,0 +1,8 @@
+package main
+
+func checkIfHoliday() {
+	//Check holiday list if case match, return true
+}
+func checkBrokerageResponding() {
+	//Query brokerage and if error response or null, return false
+}
